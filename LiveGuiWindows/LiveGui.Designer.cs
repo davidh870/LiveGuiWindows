b31426ce89abffd53aae7cccc00e93c3f657da01
davidh870 <davidh870@gmail.com>
2024-03-12 16:38:32 -0500

    Add project files.

---
+++ LiveGuiWindows/LiveGui.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace LiveGuiWindows
+{
+    partial class LiveGui
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.arduinoSerialPort = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // arduinoSerialPort
+            // 
+            this.arduinoSerialPort.PortName = "COM3";
+            // 
+            // LiveGui
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "LiveGui";
+            this.Text = "Live Gui";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort arduinoSerialPort;
+    }
+}
+
---
+++ LiveGuiWindows/LiveGui.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LiveGuiWindows
+{
+    public partial class LiveGui : Form
+    {
+        public LiveGui()
+        {
+            InitializeComponent();
+
+            // Opens and initializes arduino serial port at COM6
+            arduinoSerialPort.Open(); 
+        }
+    }
+}
